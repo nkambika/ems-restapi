ccf5aa72c6a6068b23186d389db69b14e8bd566f
nkambika <nkambika00@gmail.com>
2025-03-06 15:26:31 +0530

    project setup

---
+++ src/main/java/com/myprojects/ems/EmsRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsRestapiApplication.class, args);
+	}
+
+}
